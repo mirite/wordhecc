536814ee9aaf46c47074f316407e8d21388476f5
Jesse Conner <jconner90@gmail.com>
2022-03-26 11:42:50 -0400

    Fix a bad condition

--- src/api/check/check.ts
+++ src/api/check/check.ts
@@ -5,7 +5,7 @@ export const handler: Handler = async (event, context) => {
 	try {
 		if (!event.body) return { statusCode: 400 };
 		const { attempt } = JSON.parse(event.body);
-		if (!attempt || attempt.length <= 10) return { statusCode: 400 };
+		if (!attempt || attempt.length > 9) return { statusCode: 400 };
 		return {
 			statusCode: 200,
 			body: JSON.stringify({
