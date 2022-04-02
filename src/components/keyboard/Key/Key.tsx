0697d7e37086b6ed701e38cd8491ac3df21a490d
Jesse Conner <jconner90@gmail.com>
2022-04-02 10:33:19 -0400

    Fix keyboard style loading

--- src/components/keyboard/Key/Key.tsx
+++ src/components/keyboard/Key/Key.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import * as styles from './Key.module.css';
 import { stateClasses, ILetter } from '../../../types';
 
 interface IProps {
@@ -12,9 +11,7 @@ const Key = (props: IProps) => {
 	return (
 		<button
 			type="button"
-			className={
-				'btn btn-primary ' + styles.keyboardKey + ' ' + stateClass
-			}
+			className={'btn btn-primary letter ' + stateClass}
 			onClick={props.onClick}
 		>
 			{props.letter.character}
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,3 @@
-import * as styles from './styles/Letter.module.css';
-
 export enum ELetterState {
 	unused,
 	notInWord,
@@ -22,8 +20,8 @@ export interface IKeyboard extends Array<ILetter> {}
 export interface IAttempt extends Array<ILetter> {}
 
 export const stateClasses: { [key in ELetterState]: string } = {
-	[ELetterState.unused]: styles.unused,
-	[ELetterState.notInWord]: styles.notInWord,
-	[ELetterState.inPosition]: styles.inPosition,
-	[ELetterState.inWord]: styles.inWord,
+	[ELetterState.unused]: 'unused',
+	[ELetterState.notInWord]: 'notInWord',
+	[ELetterState.inPosition]: 'inPosition',
+	[ELetterState.inWord]: 'inWord',
 };
