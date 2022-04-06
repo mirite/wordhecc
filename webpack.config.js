e7a2e3d6c8fbad3e51c75dc50977d21d205b1357
Jesse Conner <jconner90@gmail.com>
2022-04-06 13:03:16 -0400

    Uploading source maps

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HoneybadgerWebpack = require('@honeybadger-io/webpack');
 
 module.exports = {
 	entry: {
@@ -7,21 +8,21 @@ module.exports = {
 	},
 	module: {
 		rules: [
-			{test: /\.tsx?$/, loader: 'ts-loader'},
+			{ test: /\.tsx?$/, loader: 'ts-loader' },
 			{
 				test: /\.scss$/,
 				use: [
-					{loader: 'style-loader'},
-					{loader: 'css-loader'},
-					{loader: 'sass-loader'},
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' },
 				],
 			},
 			{
 				test: /\.css$/,
 				use: [
-					{loader: 'style-loader'}, // to inject the result into the DOM as a style block
-					{loader: 'css-modules-typescript-loader'}, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
-					{loader: 'css-loader', options: {modules: true}}, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
+					{ loader: 'style-loader' }, // to inject the result into the DOM as a style block
+					{ loader: 'css-modules-typescript-loader' }, // to generate a .d.ts module next to the .scss file (also requires a declaration.d.ts with "declare modules '*.scss';" in it to tell TypeScript that "import styles from './styles.scss';" means to load the module "./styles.scss.d.td")
+					{ loader: 'css-loader', options: { modules: true } }, // to convert the resulting CSS to Javascript to be bundled (modules:true to rename CSS classes in output to cryptic identifiers, except if wrapped in a :global(...) pseudo class)
 				],
 			},
 		],
@@ -41,6 +42,14 @@ module.exports = {
 	plugins: [
 		new HtmlWebpackPlugin({
 			template: 'src/static/index.html',
+		}),
+		new HoneybadgerWebpack({
+			apiKey: 'hbp_ENX1M79SimYcIlUO4QtdeuxTx3xSIm4xeeVI',
+			assetsUrl: 'dist/wordhecc.bundle.js',
+			deploy: {
+				environment: process.env.NODE_ENV,
+				repository: 'https://github.com/mirite/wordhecc',
+			},
 		}),
 	],
 };
