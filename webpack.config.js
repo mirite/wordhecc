cbe062ae706e96b1aa79bfcd3ab769830d929fe9
Jesse Conner <jconner90@gmail.com>
2022-04-06 13:49:24 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HoneybadgerWebpack = require('@honeybadger-io/webpack');
+require('dotenv').config();
 
 module.exports = {
 	entry: {
